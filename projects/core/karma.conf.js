ce0325abe4fdadc3103ca248ca4e429d6ca79395
Daniel Schuba <daniel.schuba@kps.com>
2019-06-06 12:34:07 +0200

    Fixed testing on CI

--- projects/core/karma.conf.js
+++ projects/core/karma.conf.js
@@ -12,6 +12,12 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    customLaunchers: {
+      ChromeCustom: {
+        base: 'ChromeCI',
+        flags: ['--no-sandbox']
+      }
+    },
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
