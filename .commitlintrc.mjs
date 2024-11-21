744fa2dcbc11f6769662945d149311dc58dc5dcd
Daniel Schuba <daniel.schuba@footage.one>
2024-11-21 16:31:38 +0100

    fix(deps): adjust peer dependencies

---
+++ .commitlintrc.mjs
@@ -0,0 +1,11 @@
+import nxScopes from '@commitlint/config-nx-scopes';
+
+export default {
+  rules: {
+    'scope-enum': (ctx) => [
+      2,
+      'always',
+      ['deps', 'deps-dev', ...(nxScopes.utils.getProjects(ctx))],
+    ],
+  },
+};
